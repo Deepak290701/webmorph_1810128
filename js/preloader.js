e34c8a6711716c163fb923f2eea6432e7a12e7ff
Deepak290701 <50839672+Deepak290701@users.noreply.github.com>
2020-04-24 21:46:04 +0530

    Add files via upload

--- js/preloader.js
+++ js/preloader.js
@@ -1,7 +1,7 @@
 
 var myVar;
 
-function myFunction() {
+function myFunction1() {
   myVar = setTimeout(showPage, 5000);
 }
 
@@ -9,4 +9,13 @@ function showPage() {
   document.getElementById("loader-text").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 
+}
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 }
